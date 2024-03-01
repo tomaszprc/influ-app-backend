17b35ddff1d2f16c5dee08ddd372abec10ce2c68
Tomasz Prochowicz <t.prochowicz@selleo.com>
2024-03-01 15:33:13 +0100

    feat: add company routes for app

---
+++ app/Http/Controllers/Api/V1/CompanyController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCompanyRequest;
+use App\Http\Requests\UpdateCompanyRequest;
+use App\Http\Resources\CompanyResource;
+use App\Models\Company;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    { 
+        return CompanyResource::collection(Company::all());
+    }  
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }    
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCompanyRequest $request)
+    {
+        $company = Company::create($request->all());
+        error_log($company);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Company $company)
+    {
+        return response()->json($company);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Company $company)
+    {
+        //
+    }
+
+    public function kupa() {
+        return "kupa";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCompanyRequest $request, Company $company)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Company $company)
+    {
+        //
+    }
+}

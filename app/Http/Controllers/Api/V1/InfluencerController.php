0cc7aef86d966cf0e5f0074044f60781b4a35850
Tomasz Prochowicz <t.prochowicz@selleo.com>
2024-03-14 23:34:29 +0100

    feat: remove useless columns for user

--- app/Http/Controllers/Api/V1/InfluencerController.php
+++ app/Http/Controllers/Api/V1/InfluencerController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreInfluencerRequest;
 use App\Http\Requests\UpdateInfluencerRequest;
 use App\Models\Influencer;
